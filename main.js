eba6b089e2061fdd15a5c093821df387df6e76fd
H_mcg <82006254+Hmcg23@users.noreply.github.com>
2021-08-03 09:51:34 -0700

    add responsive nav. Will fix bugs later

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+const paperPlane = document.getElementsByClassName('paper-plane');
+
+
 const flightPath = {
     curviness: 1.5,
     autoRotate: true,
     values: [
-        {x: 450, y: 50}, {x: 700, y: 350}, {x: 680, y: 400}, {x: 850, y: 650}, {x: 1300, y: 350}, {x: 680, y: 0}, {x: 600, y: 500}, {x: 1200, y: 50}, {x: window.innerWidth + 200, y: 150}
+        {x: 450, y: 50}, {x: 700, y: 350}, {x: 680, y: 400}, {x: 850, y: 500}, {x: 1300, y: 350}, {x: 680, y: 0}, {x: 600, y: 500}, {x: 1200, y: 50}, {x: window.innerWidth + 200, y: 150}
     ]
 }
 
@@ -14,3 +17,30 @@ tween.add(
         ease: Power1.easeInOut
     })
 );
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelector('.navlinks li')
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 1.5}s`;
+        }
+        });
+        // Burger Animation
+        burger.classList.toggle('toggle');
+
+
+    });
+
+
+}
+
+navSlide();
